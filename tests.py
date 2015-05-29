4aa823ecc75360882cbdd3fce9cf4dbf56ec9c50
Marcela Hrda <marcelka.hrda@gmail.com>
2015-05-29 16:22:08 +0200

    Removing unnecessary creation of test suite.

--- tests.py
+++ tests.py
@@ -5,11 +5,4 @@ from sort import TestSortLibrary
 import unittest
 
 
-test_suite = unittest.TestSuite()
-test_suite.addTest(TestDijkstra("test_dijkstra"))
-test_suite.addTest(TestMaxFlow("test_max_flow"))
-test_suite.addTest(TestPrimesLibrary("test_by_comparison"))
-test_suite.addTest(TestSortLibrary("test_mergesort_random"))
-test_suite.addTest(TestSortLibrary("test_mergesort"))
-
 unittest.main()
