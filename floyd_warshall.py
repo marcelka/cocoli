f41f5dd282c9281eff8a3c7f6c4078e6a489454d
Marcela Hrda <marcelka.hrda@gmail.com>
2015-05-30 16:00:50 +0200

    Floyd-Warshall algorithm returns also next moves.

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -10,8 +10,13 @@ def directed(undirected_weights):
 
 def shortest_distances(weights):
     """
-    Returns shortest path values in a directed graph for each pair of vertices
-    using Floyd-Warshall algorithm.
+    Returns shortest path values and next move along this path in a directed
+    graph for each pair of vertices using Floyd-Warshall algorithm. For example,
+    if the shortest path from vertex "v1" to vertex "v2" is ("v1"->"v3",
+    "v3"->"v4", "v4"->"v1") and has length 7, result["v1"]["v2"] will be equal
+    to (7, "v3"). If there are multiple next moves leading to same (minimum)
+    distance, one of them (randomly chosen) will be returned.
+
     Arguments:
     weights -- dict of dicts such that weights[v1][v2] = w iff there is an edge
                from v1 to v2 with weight w
@@ -23,33 +28,45 @@ def shortest_distances(weights):
     id2v = dict(enumerate(vertices))
     ids = id2v.keys()
 
-    result = [[None for v in vertices] for w in vertices]
+    distance = [[None for v in vertices] for w in vertices]
+    next_move = [[None for v in vertices] for w in vertices]
 
     for id1, id2 in product(ids, repeat=2):
         v1, v2 = id2v[id1], id2v[id2]
         if v1 in weights and v2 in weights[v1]:
-            result[id1][id2] = weights[v1][v2]
+            distance[id1][id2] = weights[v1][v2]
+            next_move[id1][id2] = id2
         else:
-            result[id1][id2] = None
-        result[id1][id1] = 0
+            distance[id1][id2] = None
+        distance[id1][id1] = 0
 
     for new in ids:
         for id1, id2 in product(ids, repeat=2):
-            if result[id1][new] != None and result[new][id2] != None:
-                via_new = result[id1][new] + result[new][id2]
-                if result[id1][id2] == None:
-                    result[id1][id2] = via_new
-                else:
-                    result[id1][id2] = min(result[id1][id2], via_new)
+            if distance[id1][new] != None and distance[new][id2] != None:
+                via_new = distance[id1][new] + distance[new][id2]
+                if distance[id1][id2] == None or via_new < distance[id1][id2]:
+                    distance[id1][id2] = via_new
+                    next_move[id1][id2] = next_move[id1][new]
 
-    vertex_result = dict((v, {}) for v in vertices)
+    dist_res = dict((v, {}) for v in vertices)
+    nm_res = dict((v, dict((v, None) for v in vertices)) for v in vertices)
+
     for id1, id2 in product(ids, repeat=2):
         v1, v2 = id2v[id1], id2v[id2]
-        vertex_result[v1][v2] = result[id1][id2]
+        if next_move[id1][id2] != None:
+            nm_res[v1][v2] = id2v[next_move[id1][id2]]
+        dist_res[v1][v2] = distance[id1][id2]
 
-    return vertex_result
+    return dist_res, nm_res
 
-def has_cycle(graph):
+def path(start, end, next_moves):
+    path = [start]
+    while True:
+        if next_moves[path[-1]] == None: return None
+        path.append(next_moves[path[-1]][end])
+        if path[-1] == end: return path
+
+def find_cycle(graph):
     """Returns True iff the given directed graph has a cycle.
 
     Arguments:
@@ -61,8 +78,9 @@ def has_cycle(graph):
     for v1 in graph:
         for v2 in graph[v1]:
             weights[v1][v2] = -1
-    distances = shortest_distances(weights)
-    return any(distances[v][v] < 0 for v in graph)
+    dists, next_moves = shortest_distances(weights)
+    for v in graph:
+        if dists[v][v] < 0: return path(v, v, next_moves)
 
 
 from itertools import combinations
@@ -74,12 +92,12 @@ class TestFloydWarshall(unittest.TestCase):
 
     def test_floyd_warshall(self):
         weights = {1: {2: 100, 3: 10}, 3: {2: 20, 4: 1}, 4: {2: 2}}
-        result = shortest_distances(weights)
-        self.assertEqual(result[1][1], 0)
-        self.assertEqual(result[1][2], 13)
-        self.assertEqual(result[1][3], 10)
-        self.assertEqual(result[1][4], 11)
-        self.assertEqual(result[2][3], None)
+        dist, next_move = shortest_distances(weights)
+        self.assertEqual((dist[1][1], next_move[1][1]), (0, None))
+        self.assertEqual((dist[1][2], next_move[1][2]), (13, 3))
+        self.assertEqual((dist[1][3], next_move[1][3]), (10, 3))
+        self.assertEqual((dist[1][4], next_move[1][4]), (11, 3))
+        self.assertEqual((dist[2][3], next_move[2][3]), (None, None))
 
     def test_by_comparison(self):
         VERTEX_COUNT = 20
@@ -94,11 +112,12 @@ class TestFloydWarshall(unittest.TestCase):
         fw_result = shortest_distances(weights)
         for v1, v2 in product(vertices, repeat=2):
             dijkstra_result = dijkstra.shortest_path(neighs, v1, v2)[0]
-            self.assertEqual(fw_result[v1][v2], dijkstra_result)
+            self.assertEqual(fw_result[0][v1][v2], dijkstra_result)
 
     def test_cycle_detection(self):
-        self.assertFalse(has_cycle({1: [2], 2: [3, 4], 3: [5]}))
-        self.assertTrue(has_cycle({1: [2], 2: [3, 4], 4: [1], 3: [5]}))
+        self.assertEqual(find_cycle({1: [2], 2: [3, 4], 3: [5]}), None)
+        self.assertEqual(tuple(find_cycle({1: [2], 2: [3, 4], 4: [1], 3: [5]})),
+                         (1, 2, 4, 1))
 
 
 if __name__ == '__main__':
