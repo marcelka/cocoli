e5fc396f04751395eb8a58c31ba3ce904d77b6a8
Marcela Hrda <marcelka.hrda@gmail.com>
2015-06-03 01:49:30 +0200

    Changing max flow interface and implementation.

--- max_flow.py
+++ max_flow.py
@@ -1,56 +1,70 @@
 from collections import namedtuple, defaultdict
-from structures import SimpleQueue
-
-Edge = namedtuple('Edge', ['start', 'end'])
+try:
+    from queue import Queue
+except ImportError:
+    from Queue import Queue
 
 def augmenting_path(neighs, source, drain):
     visited = set()
-    to_visit = SimpleQueue([source])
+    to_visit = Queue()
+    to_visit.put(source)
     prevs = {}
     while True:
         if drain in visited: return path(source, drain, prevs)
-        if not to_visit: return None
-        v = to_visit.pop()
-        for n in neighs(v):
-            if n not in visited and n not in to_visit:
-                to_visit.push(n)
-                prevs[n] = v
+        if to_visit.empty(): return None, 0
+        v = to_visit.get()
+        if v in visited: continue
+        for n, cap in neighs(v):
+            if n not in visited:
+                to_visit.put(n)
+                prevs[n] = (v, cap)
         visited.add(v)
 
 def path(start, end, prevs):
-    path = [Edge(prevs[end], end)]
+    path = [end]
+    capacity = prevs[end][1]
     while True:
-        last = path[-1][0]
-        if last == start: return list(reversed(path))
-        path.append(Edge(prevs[last], last))
-
-def residual_graph_neighs(capacity, flow):
-    neighs = defaultdict(set)
-    rgcapacity = {}
-    for e in capacity:
-        flowing = flow[e] if e in flow else 0
-        if capacity[e] > flowing:
-            neighs[e.start].add(e.end)
-            rgcapacity[e] = capacity[e] - flowing
-        if flowing > 0:
-            neighs[e.end].add(e.start)
-            rgcapacity[Edge(e.end, e.start)] = flowing
-    return neighs, rgcapacity
+        last = path[-1]
+        if last == start: return list(reversed(path)), capacity
+        capacity = min(capacity, prevs[last][1])
+        path.append(prevs[last][0])
 
 def max_flow(capacity, source, drain):
-    """Capacity: dict from Edge to max flow capacity for that edge."""
-    """Returns max flow and its value."""
-    flow = {}
-    for e in capacity: flow[e] = 0
+    """
+    Returns max flow and its value.
+
+    Arguments:
+    capacity -- dict of dicts such that capacity[v1][v2] is the flow capacity of
+                edge from v1 to v2
+    source -- the source vertex
+    drain -- the drain vertex
+    """
+    vertices = set(capacity.keys())
+    for v in capacity:
+        vertices.update(capacity[v].keys())
+
+    _neighs = defaultdict(set)
+    for v in capacity:
+        for w in capacity[v]:
+            _neighs[v].add(w)
+            _neighs[w].add(v)
+
+    flow = dict((v1, dict((v2, 0) for v2 in vertices)) for v1 in vertices)
+
     while True:
-        rgneighs, rgcapacity = residual_graph_neighs(capacity, flow)
-        aug_path = augmenting_path(lambda v: rgneighs[v], source, drain)
+        def residual_graph_neighs(v):
+            for w in _neighs[v]:
+                c = (capacity[v][w] if v in capacity and w in capacity[v] else 0) - flow[v][w]
+                if c>0: yield (w, c)
+
+        aug_path, amount = augmenting_path(residual_graph_neighs, source, drain)
         if aug_path == None:
-            return flow, sum(flow[e] for e in flow if e[0] == source)
-        amount = min(rgcapacity[e] for e in aug_path)
-        for e in aug_path:
-            if e in flow: flow[e] += amount
-            else: flow[Edge(e.end, e.start)] -= amount
+            return flow, sum(flow[source].values())
+
+        for i in range(len(aug_path) - 1):
+            v1, v2 = aug_path[i], aug_path[i+1]
+            flow[v1][v2] += amount
+            flow[v2][v1] -= amount
 
 
 import unittest
@@ -58,20 +72,18 @@ import unittest
 class TestMaxFlow(unittest.TestCase):
 
   def test_max_flow(self):
-      capacity = {Edge("s", 1): 10, Edge(1, "d"): 6}
+      capacity = {"s": {1: 10}, 1: {"d": 6}}
       self.assertEqual(max_flow(capacity, "s", "d")[1], 6)
 
-      capacity = {Edge("s", 1): 10, Edge(2, "d"): 6}
+      capacity = {"s": {1: 10}, 2: {"d": 6}}
       self.assertEqual(max_flow(capacity, "s", "d")[1], 0)
 
       capacity = {
-          Edge("s", 1): 10,
-          Edge("s", 3): 4,
-          Edge(1, 2): 13,
-          Edge(1, 4): 4,
-          Edge(2, "d"): 10,
-          Edge(3, 2): 4,
-          Edge(4, "d"): 4,
+          "s": {1: 10, 3: 4},
+          1: {2: 13, 4: 4},
+          2: {"d": 10},
+          3: {2: 4},
+          4: {"d": 4},
       }
       self.assertEqual(max_flow(capacity, "s", "d")[1], 14)
 
